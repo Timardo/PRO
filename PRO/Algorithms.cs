3a4b82372b064a38a124a1fdbdc26fc5cfb610f5
Timardo <33033989+Timardo@users.noreply.github.com>
2019-11-13 10:05:13 +0100

    clean

--- PRO/Algorithms.cs
+++ PRO/Algorithms.cs
@@ -7,111 +7,6 @@ namespace PRO
 {
     class Algorithms
     {
-        internal static string Nahrad(string text)
-        {
-            string znak = text.Substring(0, 1);
-            string nahradit = text.Substring(1);
-            nahradit = nahradit.Replace(znak, "#");
-            return znak + nahradit;
-        }
-
-        internal static string NahradPosledny(string text)
-        {
-            try
-            {
-                string[] split = text.Split(';');
-                string retazec = split[0];
-                string sub = split[1];
-                string nahrada = split[2];
-
-                string[] bezNahrady = retazec.Split(new string[] { sub }, StringSplitOptions.None);
-                int amount = bezNahrady.Length - 1;
-                int lastIndexOfSub = retazec.Length - bezNahrady.Last().Length - sub.Length; //index posledného výskytu sub v reťazci
-                string posledny = retazec.Substring(lastIndexOfSub); //obsahuje posledný výskyt subu a ostatok, čo je za ním
-                posledny = posledny.Replace(sub, nahrada);
-                string _out = retazec.Substring(0, lastIndexOfSub) + posledny;
-                return _out;
-            }
-
-            catch
-            {
-                return "Ďalej to už nepôjde, formát: pôvodny-reťazec;hľadaný-podreťazec;náhrada";
-            }
-        }
-
-        internal static string Samohlasky(string text)
-        {
-            int count = text.ToLower().Count(c => c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y' || c == 'á' || c == 'ä' || c == 'é' || c == 'í' || c == 'ó' || c == 'ú' || c == 'ý');
-            return "Veta '" + text + "' má " + count + " samohlások";
-        }
-
-        internal static string CMH(string text)
-        {
-            try
-            {
-                string[] vars = text.Split(';');
-                string first = vars[0];
-                string second = vars[1];
-                string[] split = first.Split('*');
-                string znak = second.Replace(split[0], "").Replace(split[1], "");
-
-                if (first.Replace("*", znak) == second)
-                    return "ANO";
-
-                return "NIE";
-            }
-
-            catch
-            {
-                return "Ďalej to už nepôjde, formát: prvý-riadok;druhý-riadok";
-            }
-        }
-
-        internal static string PrvePismena(string text)
-        {
-            string[] split = text.Split(' ');
-            string _out = "";
-
-            foreach (string s in split)
-                _out += s[0];
-
-            return _out;
-        }
-
-        internal static string Zhoda(string text)
-        {
-            try
-            {
-                string[] split = text.Split(';');
-                string zhoda = "None";
-                int max = 0;
-
-                for (int i = 0; i < split[0].Length; i++)
-                {
-                    for (int j = split[0].Length - i; j > 0; j--)
-                    {
-                        string sub = split[0].Substring(i, j);
-
-                        if (split[1].Contains(sub))
-                        {
-                            if (sub.Length > max)
-                            {
-                                max = sub.Length;
-                                zhoda = sub;
-                            }
-                        }
-                    }
-                }
-
-                return zhoda;
-            }
-
-            catch
-            {
-                return "Ďalej to už nepôjde, správny formát: veta1;veta2";
-            }
-        }
-
         internal static string Slovakize(string text)
         {
             return text.Replace('0', 'é').Replace('1', '+').Replace('2', 'ľ').Replace('3', 'š').Replace('4', 'č').Replace('5', 'ť').Replace('6', 'ž').Replace('7', 'ý').Replace('8', 'á').Replace('9', 'í');
@@ -139,15 +34,15 @@ namespace PRO
                 foreach (char c in first)
                 {
                     if (second == "")
-                        return "FALSE";
+                        return "FALSE - slová nie sp anagramy";
 
                     second = second.Substring(0, second.IndexOf(c)) + second.Substring(second.IndexOf(c) + 1);
                 }
 
                 if (second == "")
-                    return "TRUE";
+                    return "TRUE - slová sú anagramy";
 
-                 return "FALSE";
+                 return "FALSE - slová nie sú anagramy";
             }
 
             catch
@@ -165,146 +60,6 @@ namespace PRO
                 _out += parts[i].ToUpper()[0] + ". ";
 
             return _out + parts.Last().ToUpper()[0] + parts.Last().Substring(1);
-        }
-
-        internal static string Otacac(string text)
-        {
-            try
-            {
-                string[] split = text.Split(';');
-                string veta = split[0];
-                int vetaLen = veta.Length;
-                int pocet = int.Parse(split[1]);
-                pocet = pocet < 0 ? vetaLen + pocet : pocet;
-            }
-
-            catch
-            {
-
-            }
-
-            return "ešte nefunguje";
-        }
-
-        internal static string Replace(string text)
-        {
-            try
-            {
-                string[] split = text.Split(';');
-                char[] veta = split[0].ToCharArray();
-                char stare = split[1][0];
-                char nove = split[2][0];
-
-                //return veta.Replace(stare, nove);
-
-                for (int i = 0; i < veta.Length; i++)
-                    if (veta[i] == stare)
-                        veta[i] = nove;
-
-                return new string(veta);
-            }
-
-            catch
-            {
-                return "Ďalej to už nepôjde, správny formát: veta;stary-znak;novy-znak";
-            }
-        }
-
-        internal static string Zdvoj(string text)
-        {
-            string _out = "";
-
-            foreach (char ch in text)
-                if (ch != ' ')
-                    _out += new string(ch, 2);
-
-                else
-                    _out += " ";
-
-            return _out;
-        }
-
-        internal static string Soupa(string text)
-        {
-            int count = text.Count(c =>
-                c == 'A' ||
-                c == 'D' ||
-                c == 'O' ||
-                c == 'P' ||
-                c == 'Q' ||
-                c == 'R' ||
-                c == 'a' ||
-                c == 'b' ||
-                c == 'd' ||
-                c == 'e' ||
-                c == 'g' ||
-                c == 'o' ||
-                c == 'p' ||
-                c == 'q' ||
-                c == '4' ||
-                c == '6' ||
-                c == '9' ||
-                c == '0') + 2 * text.Count(c =>
-                c == 'B' ||
-                c == '8');
-
-            return count.ToString();
-        }
-
-        internal static string Samostatne(string text)
-        {
-            string _out = "Tieto znaky sú v texte iba raz: ";
-
-            foreach (char ch in text.ToLower())
-            {
-                if (text.Count(c => c == ch) == 1)
-                    _out += ch + ", ";
-            }
-
-            return _out;
-        }
-
-        internal static string ObratVetu(string text)
-        {
-            int firstIndex = text.IndexOfAny(new char[] { '.', '?', '!' });
-            string punct = "";
-
-            if (firstIndex != -1)
-            {
-                punct = text.Substring(firstIndex);
-                text = text.Substring(0, firstIndex);
-            }
-
-            string[] split = text.Split(' ');
-
-            //string _out = split.Reverse().ToArray();
-            string _out = "";
-
-            for (int i = split.Length - 1; i >= 0; i--)
-            {
-                if (i != 0)
-                    _out += split[i] + " ";
-                else
-                    _out += split[0] + punct;
-            }
-
-            return _out;
-        }
-
-        internal static string AkoVeta(string text)
-        {
-            string[] slova = text.Split(' ');
-
-            for (int i = 0; i < slova.Length; i++)
-            {
-                if (i == 0 || slova[i - 1].EndsWith(".") || slova[i - 1].EndsWith("!") || slova[i - 1].EndsWith("?"))
-                    slova[i] = slova[i].First().ToString().ToUpper() + slova[i].Substring(1).ToLower();
-
-                else
-                    slova[i] = slova[i].ToLower();
-            }
-
-            return string.Join(" ", slova);
         }
     }
 }
--- PRO/Form1.Designer.cs
+++ PRO/Form1.Designer.cs
@@ -35,19 +35,6 @@
             this.Output = new System.Windows.Forms.TextBox();
             this.Clear = new System.Windows.Forms.Button();
             this.ShowOutput = new System.Windows.Forms.Button();
-            this.Nahrad = new System.Windows.Forms.Button();
-            this.NahradPosledny = new System.Windows.Forms.Button();
-            this.AkoVeta = new System.Windows.Forms.Button();
-            this.ObratVetu = new System.Windows.Forms.Button();
-            this.Samohlasky = new System.Windows.Forms.Button();
-            this.SamostatneZnaky = new System.Windows.Forms.Button();
-            this.ZenitOne = new System.Windows.Forms.Button();
-            this.Soupa = new System.Windows.Forms.Button();
-            this.Zdvoj = new System.Windows.Forms.Button();
-            this.PrvePismena = new System.Windows.Forms.Button();
-            this.Menic = new System.Windows.Forms.Button();
-            this.Zhoda = new System.Windows.Forms.Button();
-            this.Otacac = new System.Windows.Forms.Button();
             this.Inicialy = new System.Windows.Forms.Button();
             this.Anagramy = new System.Windows.Forms.Button();
             this.yToZToY = new System.Windows.Forms.Button();
@@ -119,139 +106,9 @@
             this.ShowOutput.UseVisualStyleBackColor = true;
             this.ShowOutput.Click += new System.EventHandler(this.ShowOutput_Click);
             // 
-            // Nahrad
-            // 
-            this.Nahrad.Location = new System.Drawing.Point(64, 39);
-            this.Nahrad.Name = "Nahrad";
-            this.Nahrad.Size = new System.Drawing.Size(88, 23);
-            this.Nahrad.TabIndex = 13;
-            this.Nahrad.Text = "1. Nahraď prvý";
-            this.Nahrad.UseVisualStyleBackColor = true;
-            this.Nahrad.Click += new System.EventHandler(this.Nahrad_Click);
-            // 
-            // NahradPosledny
-            // 
-            this.NahradPosledny.Location = new System.Drawing.Point(158, 39);
-            this.NahradPosledny.Name = "NahradPosledny";
-            this.NahradPosledny.Size = new System.Drawing.Size(110, 23);
-            this.NahradPosledny.TabIndex = 14;
-            this.NahradPosledny.Text = "2. Nahraď posledný";
-            this.NahradPosledny.UseVisualStyleBackColor = true;
-            this.NahradPosledny.Click += new System.EventHandler(this.NahradPosledny_Click);
-            // 
-            // AkoVeta
-            // 
-            this.AkoVeta.Location = new System.Drawing.Point(274, 39);
-            this.AkoVeta.Name = "AkoVeta";
-            this.AkoVeta.Size = new System.Drawing.Size(73, 23);
-            this.AkoVeta.TabIndex = 15;
-            this.AkoVeta.Text = "3. Ako veta";
-            this.AkoVeta.UseVisualStyleBackColor = true;
-            this.AkoVeta.Click += new System.EventHandler(this.AkoVeta_Click);
-            // 
-            // ObratVetu
-            // 
-            this.ObratVetu.Location = new System.Drawing.Point(353, 39);
-            this.ObratVetu.Name = "ObratVetu";
-            this.ObratVetu.Size = new System.Drawing.Size(79, 23);
-            this.ObratVetu.TabIndex = 16;
-            this.ObratVetu.Text = "4. Obráť Vetu";
-            this.ObratVetu.UseVisualStyleBackColor = true;
-            this.ObratVetu.Click += new System.EventHandler(this.ObratVetu_Click);
-            // 
-            // Samohlasky
-            // 
-            this.Samohlasky.Location = new System.Drawing.Point(438, 39);
-            this.Samohlasky.Name = "Samohlasky";
-            this.Samohlasky.Size = new System.Drawing.Size(116, 23);
-            this.Samohlasky.TabIndex = 17;
-            this.Samohlasky.Text = "6. Počet Samohlások";
-            this.Samohlasky.UseVisualStyleBackColor = true;
-            this.Samohlasky.Click += new System.EventHandler(this.Samohlasky_Click);
-            // 
-            // SamostatneZnaky
-            // 
-            this.SamostatneZnaky.Location = new System.Drawing.Point(560, 39);
-            this.SamostatneZnaky.Name = "SamostatneZnaky";
-            this.SamostatneZnaky.Size = new System.Drawing.Size(116, 23);
-            this.SamostatneZnaky.TabIndex = 18;
-            this.SamostatneZnaky.Text = "7. Samostatné znaky";
-            this.SamostatneZnaky.UseVisualStyleBackColor = true;
-            this.SamostatneZnaky.Click += new System.EventHandler(this.SamostatneZnaky_Click);
-            // 
-            // ZenitOne
-            // 
-            this.ZenitOne.Location = new System.Drawing.Point(682, 39);
-            this.ZenitOne.Name = "ZenitOne";
-            this.ZenitOne.Size = new System.Drawing.Size(89, 23);
-            this.ZenitOne.TabIndex = 19;
-            this.ZenitOne.Text = "8. Zenit 18";
-            this.ZenitOne.UseVisualStyleBackColor = true;
-            this.ZenitOne.Click += new System.EventHandler(this.ZenitOne_Click);
-            // 
-            // Soupa
-            // 
-            this.Soupa.Location = new System.Drawing.Point(64, 68);
-            this.Soupa.Name = "Soupa";
-            this.Soupa.Size = new System.Drawing.Size(114, 23);
-            this.Soupa.TabIndex = 20;
-            this.Soupa.Text = "9. Gaučová Funkcia";
-            this.Soupa.UseVisualStyleBackColor = true;
-            this.Soupa.Click += new System.EventHandler(this.Soupa_Click);
-            // 
-            // Zdvoj
-            // 
-            this.Zdvoj.Location = new System.Drawing.Point(184, 68);
-            this.Zdvoj.Name = "Zdvoj";
-            this.Zdvoj.Size = new System.Drawing.Size(91, 23);
-            this.Zdvoj.TabIndex = 21;
-            this.Zdvoj.Text = "Zdvoj písmená";
-            this.Zdvoj.UseVisualStyleBackColor = true;
-            this.Zdvoj.Click += new System.EventHandler(this.Zdvoj_Click);
-            // 
-            // PrvePismena
-            // 
-            this.PrvePismena.Location = new System.Drawing.Point(281, 68);
-            this.PrvePismena.Name = "PrvePismena";
-            this.PrvePismena.Size = new System.Drawing.Size(82, 23);
-            this.PrvePismena.TabIndex = 22;
-            this.PrvePismena.Text = "Prvé Písmená";
-            this.PrvePismena.UseVisualStyleBackColor = true;
-            this.PrvePismena.Click += new System.EventHandler(this.PrvePismena_Click);
-            // 
-            // Menic
-            // 
-            this.Menic.Location = new System.Drawing.Point(369, 68);
-            this.Menic.Name = "Menic";
-            this.Menic.Size = new System.Drawing.Size(45, 23);
-            this.Menic.TabIndex = 23;
-            this.Menic.Text = "Menič";
-            this.Menic.UseVisualStyleBackColor = true;
-            this.Menic.Click += new System.EventHandler(this.Menic_Click);
-            // 
-            // Zhoda
-            // 
-            this.Zhoda.Location = new System.Drawing.Point(420, 68);
-            this.Zhoda.Name = "Zhoda";
-            this.Zhoda.Size = new System.Drawing.Size(46, 23);
-            this.Zhoda.TabIndex = 24;
-            this.Zhoda.Text = "Zhoda";
-            this.Zhoda.UseVisualStyleBackColor = true;
-            this.Zhoda.Click += new System.EventHandler(this.Zhoda_Click);
-            // 
-            // Otacac
-            // 
-            this.Otacac.Location = new System.Drawing.Point(472, 68);
-            this.Otacac.Name = "Otacac";
-            this.Otacac.Size = new System.Drawing.Size(50, 23);
-            this.Otacac.TabIndex = 25;
-            this.Otacac.Text = "Otáčač";
-            this.Otacac.UseVisualStyleBackColor = true;
-            this.Otacac.Click += new System.EventHandler(this.Otacac_Click);
-            // 
             // Inicialy
             // 
-            this.Inicialy.Location = new System.Drawing.Point(528, 68);
+            this.Inicialy.Location = new System.Drawing.Point(64, 39);
             this.Inicialy.Name = "Inicialy";
             this.Inicialy.Size = new System.Drawing.Size(50, 23);
             this.Inicialy.TabIndex = 26;
@@ -261,7 +118,7 @@
             // 
             // Anagramy
             // 
-            this.Anagramy.Location = new System.Drawing.Point(584, 68);
+            this.Anagramy.Location = new System.Drawing.Point(120, 39);
             this.Anagramy.Name = "Anagramy";
             this.Anagramy.Size = new System.Drawing.Size(64, 23);
             this.Anagramy.TabIndex = 27;
@@ -271,7 +128,7 @@
             // 
             // yToZToY
             // 
-            this.yToZToY.Location = new System.Drawing.Point(654, 68);
+            this.yToZToY.Location = new System.Drawing.Point(190, 39);
             this.yToZToY.Name = "yToZToY";
             this.yToZToY.Size = new System.Drawing.Size(50, 23);
             this.yToZToY.TabIndex = 28;
@@ -281,7 +138,7 @@
             // 
             // slovakize
             // 
-            this.slovakize.Location = new System.Drawing.Point(710, 68);
+            this.slovakize.Location = new System.Drawing.Point(246, 39);
             this.slovakize.Name = "slovakize";
             this.slovakize.Size = new System.Drawing.Size(61, 23);
             this.slovakize.TabIndex = 29;
@@ -299,19 +156,6 @@
             this.Controls.Add(this.yToZToY);
             this.Controls.Add(this.Anagramy);
             this.Controls.Add(this.Inicialy);
-            this.Controls.Add(this.Otacac);
-            this.Controls.Add(this.Zhoda);
-            this.Controls.Add(this.Menic);
-            this.Controls.Add(this.PrvePismena);
-            this.Controls.Add(this.Zdvoj);
-            this.Controls.Add(this.Soupa);
-            this.Controls.Add(this.ZenitOne);
-            this.Controls.Add(this.SamostatneZnaky);
-            this.Controls.Add(this.Samohlasky);
-            this.Controls.Add(this.ObratVetu);
-            this.Controls.Add(this.AkoVeta);
-            this.Controls.Add(this.NahradPosledny);
-            this.Controls.Add(this.Nahrad);
             this.Controls.Add(this.ShowOutput);
             this.Controls.Add(this.Clear);
             this.Controls.Add(this.Output);
@@ -337,19 +181,6 @@
         private System.Windows.Forms.TextBox Output;
         private System.Windows.Forms.Button Clear;
         private System.Windows.Forms.Button ShowOutput;
-        private System.Windows.Forms.Button Nahrad;
-        private System.Windows.Forms.Button NahradPosledny;
-        private System.Windows.Forms.Button AkoVeta;
-        private System.Windows.Forms.Button ObratVetu;
-        private System.Windows.Forms.Button Samohlasky;
-        private System.Windows.Forms.Button SamostatneZnaky;
-        private System.Windows.Forms.Button ZenitOne;
-        private System.Windows.Forms.Button Soupa;
-        private System.Windows.Forms.Button Zdvoj;
-        private System.Windows.Forms.Button PrvePismena;
-        private System.Windows.Forms.Button Menic;
-        private System.Windows.Forms.Button Zhoda;
-        private System.Windows.Forms.Button Otacac;
         private System.Windows.Forms.Button Inicialy;
         private System.Windows.Forms.Button Anagramy;
         private System.Windows.Forms.Button yToZToY;
