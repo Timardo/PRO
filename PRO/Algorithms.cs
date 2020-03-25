7c24e9e2aa736c841dd5a13bc264b71a2996d0e9
Timardo <33033989+Timardo@users.noreply.github.com>
2020-03-25 12:15:52 +0100

    2
    ha

--- PRO/Algorithms.cs
+++ PRO/Algorithms.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Numerics;
 using System.Windows.Forms;
 using System.IO;
